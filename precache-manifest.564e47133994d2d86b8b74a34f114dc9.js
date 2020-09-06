8570050dd309f90b3821f4a766ad9921f76488bf
your <mhmmdkholani@gmil.com>
2020-09-06 14:41:31 +0300

    Updates

---
+++ precache-manifest.564e47133994d2d86b8b74a34f114dc9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9d209701b8db80411783fb16313b81",
+    "url": "/netlify/index.html"
+  },
+  {
+    "revision": "482fcdc98f06641e7922",
+    "url": "/netlify/static/css/2.64930493.chunk.css"
+  },
+  {
+    "revision": "05f4b681a313cdac397a",
+    "url": "/netlify/static/css/main.61733a16.chunk.css"
+  },
+  {
+    "revision": "482fcdc98f06641e7922",
+    "url": "/netlify/static/js/2.e8336592.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/netlify/static/js/2.e8336592.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05f4b681a313cdac397a",
+    "url": "/netlify/static/js/main.8863e64b.chunk.js"
+  },
+  {
+    "revision": "802f5f6fde92c68086da",
+    "url": "/netlify/static/js/runtime-main.83467f41.js"
+  },
+  {
+    "revision": "885bfb41a8fe2f8a02bcb901e6de3967",
+    "url": "/netlify/static/media/job-1.885bfb41.png"
+  },
+  {
+    "revision": "76e3582fd0b38287c84bed36a5bd269e",
+    "url": "/netlify/static/media/job-2.76e3582f.png"
+  },
+  {
+    "revision": "da157ac697f0f3187a3e6cb4253b4246",
+    "url": "/netlify/static/media/job-4.da157ac6.png"
+  },
+  {
+    "revision": "d524c01042d9d7001de064f721b30832",
+    "url": "/netlify/static/media/jop-3.d524c010.png"
+  },
+  {
+    "revision": "af40a36ad52310c6631bb624cfb4e4e2",
+    "url": "/netlify/static/media/logo.af40a36a.svg"
+  },
+  {
+    "revision": "510b649a8826cb478d6af94d83ff3400",
+    "url": "/netlify/static/media/profile.510b649a.jpg"
+  }
+]);
